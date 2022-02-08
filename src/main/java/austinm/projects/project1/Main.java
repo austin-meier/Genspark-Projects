15436c697d9f64506ea64ae6ebcb4d60e43ea7ff
Austin Meier <austin.meier@hotmail.com>
2022-02-08 07:26:21 -0700

    Added basic pattern matching and exception checking.

--- src/main/java/austinm/projects/project1/Main.java
+++ src/main/java/austinm/projects/project1/Main.java
@@ -1,6 +1,8 @@
 package austinm.projects.project1;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,10 +10,16 @@ public class Main {
 
         try (Scanner scan = new Scanner(System.in)) {
 
-            String input = scan.nextLine();
+            int i = 0;
+            Pattern p = Pattern.compile("[1-2]");
 
-            int i = Integer.parseInt(input);
             int c = (Math.random() <= 0.5) ? 1: 2;
+
+            while (i == 0) {
+                String input = scan.nextLine();
+                i = parseGuess(input, p);
+            }
+
             if (i == c) {
                 System.out.println("You approach the cave...\nIt is dark and spooky...\nA large dragon jumps out in front of you! He opens his jaws and...\nGobbles you down in one bite!");
             } else {
@@ -20,5 +28,16 @@ public class Main {
 
         }
 
+    }
+    private static int parseGuess(String input, Pattern numberPattern) {
+        try {
+            Matcher m = numberPattern.matcher(input);
+            if (m.matches()) return Integer.parseInt(input);
+            System.out.println("Not a valid input, please enter '1' or '2'");
+            return 0;
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException: Not a valid input, please enter '1' or '2'");
+            return 0;
+        }
     }
 }
